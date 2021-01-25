692bfa3985716675c452c26e24a21b6aee2a19f7
Pavel Braginskiy <pavelbraginskiy@hotmail.com>
2021-01-24 16:50:46 -0800

    test app

---
+++ ConsoleApp1/HelloForm.cs
@@ -0,0 +1,15 @@
+﻿using Eto.Drawing;
+using Eto.Forms;
+
+namespace ConsoleApp1
+{
+    public class HelloForm : Form
+    {
+        public HelloForm()
+        {
+            Title = "Hello, World!";
+            ClientSize = new Size(200, 200);
+            Content = new Label {Text = "Hello, World!"};
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Eto.Forms;
+
+// I ONLY WORK ON WINDOWS
+// ETO needs to link against platform-specific packages. I've only added Windows (at the moment). 
+// It will work fine on the non-test project
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Application().Run(new HelloForm());
+        }
+    }
+}
