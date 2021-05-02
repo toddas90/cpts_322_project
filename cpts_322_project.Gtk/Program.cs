d81f18de32db3ec07d5660db2a88b9c813060003
Pavel Braginskiy <pavelbraginskiy@hotmail.com>
2021-05-02 14:16:59 -0700

    Add eto project

---
+++ cpts_322_project.Gtk/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+
+namespace cpts_322_project.Gtk
+{
+	class MainClass
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			new Application(Eto.Platforms.Gtk).Run(new MainForm());
+		}
+	}
+}
---
+++ cpts_322_project.Wpf/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+
+namespace cpts_322_project.Wpf
+{
+	class MainClass
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			new Application(Eto.Platforms.Wpf).Run(new MainForm());
+		}
+	}
+}
